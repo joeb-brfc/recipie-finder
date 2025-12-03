9635efeda12aadcc63b8a63301850f8f4977b778
joeb_brfc <joseph_bird93@hotmail.co.uk>
2025-12-03 18:17:14 +0000

    js dom elements

---
+++ script.js
@@ -0,0 +1,13 @@
+// DOM Elements
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const mealsContainer = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const errorContainer = document.getElementById("error-container");
+const mealDetails = document.getElementById("meal-details");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const backBtn = document.getElementById("back-btn");
+
+const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
+const SEARCH_URL = `${BASE_URL}search.php?s=`;
+const LOOKUP_URL = `${BASE_URL}lookup.php?i=`;
